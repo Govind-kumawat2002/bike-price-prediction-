d427d2d6a8290d99cfb876244b2e22d74ff7c0a2
govind kumawat2002 <kumawatritik54@gmail.com>
2024-05-08 12:06:29 +0530

    bike_projects

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask , render_template # class flask in flask with function 
+from flask import Flask , render_template,url_for,request # class flask in flask with function 
+import joblib
+model =joblib.load("model.lb")
 app = Flask(__name__)
 
 @app.route('/') # home route 
@@ -13,7 +15,44 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+@app.route('/predict',methods =['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        brand_name =request.form['brand_name']
+        owner_name = int(request.form['owner'])
+        age = int(request.form['age'])
+        power = int(request.form['power'])
+        kms_driven = int(request.form['kms_driven'])
+        brand_labal = {'TVS': 0,
+                                'Royal Enfield': 1,
+                                'Triumph': 2,
+                                'Yamaha': 3,
+                                'Honda': 4,
+                                'Hero': 5,
+                                'Bajaj': 6,
+                                'Suzuki': 7,
+                                'Benelli': 8,
+                                'KTM': 9,
+                                'Mahindra': 10,
+                                'Kawasaki': 11,
+                                'Ducati': 12,
+                                'Hyosung': 13,
+                                'Harley': 14,
+                                'Jawa': 15,
+                                'BMW': 16,
+                                'Indian': 17,
+                                'Rajdoot': 18,
+                                'LML': 19,
+                                'Yezdi': 20,
+                                'MV': 21,
+                                'Ideal': 22}
+        brand_name=brand_labal[brand_name]
+        lst = [[owner_name,brand_name,kms_driven,age,power]]  #owner	brand	price	kms_driven	age	power
+        pred = model.predict(lst)
+        pred = pred[0]
+        pred = round(pred,2)
 
+        return render_template("project.html",prediction=str(pred))
 if __name__ == "__main__":
     app.run(debug= True)
 
